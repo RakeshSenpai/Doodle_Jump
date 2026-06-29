fdb9e3fb4a336d1b5dbf14471461cd7dea2d0eb0
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-29 11:31:59 +0530

    doodler move

--- script.js
+++ script.js
@@ -28,4 +28,40 @@ window.onload = function() {
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext('2d')
+
+    // Loads Images
+        doodlerRightImage = new Image();
+        doodlerRightImage.src = 'assets/images/doodler-right.png';
+        doodler.img = doodlerRightImage;
+        doodlerRightImage.onload = function(){
+        context.drawImage(doodler.img, doodler.x, doodler.y, doodler.width, doodler.height);
+
+        doodlerLeftImage = new Image()
+        doodlerLeftImage.src = 'assets/images/doodler-left.png';
+        requestAnimationFrame(update);
+        document.addEventListener('keydown', moveDoodler)
+    }
+}
+
+//Game Physics
+
+let velocityX = 0;
+
+function update(){
+    requestAnimationFrame(update)
+
+    doodler.x += velocityX;
+    context.drawImage(doodler.img, doodler.x, doodler.y, doodler.width, doodler.height)
+}
+
+function moveDoodler(e){
+    if(e.code == 'ArrowRight' || e.code == 'keyD'){
+        velocityX = 4;
+        doodler.img = doodlerRightImage;
+    }
+
+    else if(e.code == 'ArrowLeft' || e.code == 'KeyA'){
+        velocityX = -4
+        doodler.img = doodlerLeftImage;
+    }
 }
