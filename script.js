64271051ab607ac80a86af75700bf9a15d512175
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-29 10:48:37 +0530

    doodler(Player)

--- script.js
+++ script.js
@@ -5,6 +5,24 @@ let boardWidth = 360;
 let boardHeight = 576;
 let context;
 
+
+// Doodler
+
+let doodlerWidth = 46;
+let doodlerHeight = 46;
+let doodlerX = boardWidth/2 - doodlerWidth/2;
+let doodlerY = boardHeight*7/8 - doodlerHeight;
+let doodlerRightImage;
+let doodlerLeftImage;
+
+let doodler = {
+    img: null,
+    x: doodlerX,
+    y: doodlerY,
+    width: doodlerWidth,
+    height:doodlerHeight
+}
+
 window.onload = function() {
     board = document.getElementById('board')
     board.height = boardHeight;
