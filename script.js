5719b252133346114b287a3737e8f547fb376f19
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-30 11:13:45 +0530

    draw platforms

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ let doodlerY = boardHeight*7/8 - doodlerHeight;
 let doodlerRightImage;
 let doodlerLeftImage;
 
+//Platforms
+
+let platformArray = []
+let platformWidth = 60;
+let platformHeight = 18;
+let platformImg;
+
 let doodler = {
     img: null,
     x: doodlerX,
@@ -38,6 +45,12 @@ window.onload = function() {
 
         doodlerLeftImage = new Image()
         doodlerLeftImage.src = 'assets/images/doodler-left.png';
+
+        //platform
+
+        platformImg = new Image()
+        platformImg.src = 'assets/images/platform.png'
+        placePlatForms()
         requestAnimationFrame(update);
         document.addEventListener('keydown', moveDoodler)
     }
@@ -49,13 +62,27 @@ let velocityX = 0;
 
 function update(){
     requestAnimationFrame(update)
+    context.clearRect(0, 0, board.width, board.height)
 
+    //doodler 
     doodler.x += velocityX;
+
+    if(doodler.x > boardWidth){
+        doodler.x = 0
+    }
+    else if(doodler.x + doodler.width < 0){
+        doodler.x = boardWidth
+    }
     context.drawImage(doodler.img, doodler.x, doodler.y, doodler.width, doodler.height)
+
+    for(let i = 0; i < platformArray.length; i++){
+        let platform = platformArray[i];
+        context.drawImage(platform.img, platform.x, platform.y, platform.width, platform.height)
+    }
 }
 
 function moveDoodler(e){
-    if(e.code == 'ArrowRight' || e.code == 'keyD'){
+    if(e.code == 'ArrowRight' || e.code == 'KeyD'){
         velocityX = 4;
         doodler.img = doodlerRightImage;
     }
@@ -64,4 +91,18 @@ function moveDoodler(e){
         velocityX = -4
         doodler.img = doodlerLeftImage;
     }
+} 
+
+function placePlatForms(){
+    platformArray = []
+
+    let platform = {
+        img: platformImg,
+        x: boardWidth/2,
+        y : boardHeight - 50,
+        width: platformWidth,
+        height: platformHeight
+    }
+
+    platformArray.push(platform)
 }
